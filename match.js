c579f77ab7094caea7665a9a8611b77c94946e9b
Danny Guinther <dannyguinther@gmail.com>
2014-03-18 21:00:32 -0400

    Refactor

--- match.js
+++ match.js
@@ -1,17 +1,24 @@
-    var app = angular.module('myApp', []);
-    app.directive('match', function () {
-        return {
-            require: 'ngModel',
-            restrict: 'A',
-            scope: {
-                match: '='
-            },
-            link: function(scope, elem, attrs, ctrl) {
-                scope.$watch(function() {
-                    return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
-                }, function(currentValue) {
-                    ctrl.$setValidity('match', currentValue);
-                });
-            }
-        };
-    });
+(function() {
+  'use strict';
+  var inputMatch = angular.module('directives.inputMatch', []);
+  inputMatch.directive('match', function () {
+
+    function link(scope, elem, attrs, ctrl) {
+      scope.$watch(
+        function() {
+          return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
+        },
+        function(currentValue) {
+          ctrl.$setValidity('match', currentValue);
+        }
+      );
+    }
+
+    return {
+      link: link,
+      require: '?ngModel',
+      restrict: 'A',
+      scope: { match: '=' }
+    };
+  });
+})();
