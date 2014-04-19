76e5232f8cb9a8f90266aca04a27dc37165d9cca
Danny Guinther <dannyguinther@gmail.com>
2014-04-19 18:03:36 -0400

    Reorganize for better testing and distribution

--- files.js
+++ files.js
@@ -1,21 +1,54 @@
-inputMatchFiles = {
+var pkg = require('./package.json');
+
+var pkgFiles = {
   angular: [
     'bower_components/angular/angular.js'
   ],
-  build: [
-    'build/*.min.js'
+  karma: [
+    'bower_components/angular/angular.js',
+    'bower_components/angular-mocks/angular-mocks.js'
+  ],
+  'karma-build': [
+    '@karma',
+    'build/' + pkg.name + '.js',
+    '@karma-tests'
+  ],
+  'karma-min': [
+    '@karma',
+    'build/' + pkg.name + '.min.js',
+    '@karma-tests'
+  ],
+  'karma-src-exclude': [],
+  'karma-src': [
+    '@karma',
+    '@src',
+    '@karma-tests'
+  ],
+  'karma-tests': [
+    'test/**/*_test.js'
   ],
   src: [
-    'src/directives/inputMatch.js',
-  ],
-  test: [
-    "test/**/*Spec.js"
-  ],
-  testUtils: [
-    'bower_components/angular-mocks/angular-mocks.js',
+    'src/**/*.js',
   ]
 };
 
 if (exports) {
-  exports.files = inputMatchFiles;
+  exports.files = pkgFiles;
+  exports.mergeFilesFor = function() {
+    var files = [];
+
+    Array.prototype.slice.call(arguments, 0).forEach(function(filegroup) {
+      pkgFiles[filegroup].forEach(function(file) {
+        // replace @ref
+        var match = file.match(/^\@(.*)/);
+        if (match) {
+          files = files.concat(pkgFiles[match[1]]);
+        } else {
+          files.push(file);
+        }
+      });
+    });
+
+    return files;
+  };
 }
---
+++ src/angular_input_match.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var inputMatch = angular.module('directives.inputMatch', []);
+inputMatch.directive('match', function () {
+
+  function link(scope, elem, attrs, ctrl) {
+    if(!ctrl) { return; }
+
+    scope.$watch(
+      function() {
+        return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
+      },
+      function(currentValue) {
+        ctrl.$setValidity('match', currentValue);
+      }
+    );
+  }
+
+  return {
+    link: link,
+    require: '?ngModel',
+    restrict: 'A',
+    scope: { match: '=' }
+  };
+});
